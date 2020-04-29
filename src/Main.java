adc6da0a6b558ddee2c6394b9b4c22782ea5e44e
musicstudio <musicstudio@musics-MBP.home>
2020-04-28 22:16:51 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-// Point and String are classes that create or declare reference data types
-// as opposed to primitive data types.
-        // Difference between them are Point will mirror 2 variables
-        // but String does not.
+
         Point point1 = new Point(1,1);
         Point point2 = point1;
         System.out.println(point1);
@@ -24,23 +21,24 @@ public class Main {
         System.out.println(point2);
 
 
-        String message1 = new String("Hello World 1 !");
-        System.out.println(message1);
-        // or use this shorter way
-//        String message1 = "Hello World 1 !";
+        // a long way can be done but there is a short way
+//        String message1 = new String("Hello World 1 !");
+//        System.out.println(message1);
+        // this shorter way:
+        String message1 = "Hello World 1 !";
 
         String message2 = message1;
         System.out.println(message2);
 
-        //no change in this:
+
         message2.replace("!","*");
         System.out.println(message2);
 
-        // change in this but only limited in this single statement
+
         System.out.println(message2.replace("!","*"));
         System.out.println(message2);
 
-        message2 = " I only changed message 2"; // message1 does not change, unlike Point
+        message2 = " I only changed message 2";
         System.out.println(message2);
         System.out.println(message1);
 
